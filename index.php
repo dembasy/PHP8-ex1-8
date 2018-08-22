6f4f7c5cd7aa21827dc34395349ce0ca41c87853
dembasy <dembasy1227@gmail.com>
2018-08-22 14:37:18 +0200

    exercice 8 terminé

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>exercice 1</title>
+    </head>
+    <body>
+        <p>
+            <?php 
+            // utilisation de server afin d'afficher l'user agent
+                echo $_SERVER['HTTP_USER_AGENT'];
+            ?> 
+        </p>
+    </body>
+</html>
